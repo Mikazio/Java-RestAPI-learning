4d7cb7f19efd2780c4e1337efe7714a84b215e9b
kong <varin.titarat@gmail.com>
2019-07-10 09:15:24 +0700

    [Add]UserRepository file

---
+++ src/main/java/rest/pomodoro/UserRepository.java
@@ -0,0 +1,11 @@
+package rest.pomodoro;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    public User findById(long id);
+    public List<User> findByLastName(String lastname);
+    
+}
