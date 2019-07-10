a864ed5f2c6439c2b56c3261daca30b151e3528d
kong <varin.titarat@gmail.com>
2019-07-10 14:36:01 +0700

    [Update]application.properties

--- src/main/java/rest/pomodoro/PomodoroApplication.java
+++ src/main/java/rest/pomodoro/PomodoroApplication.java
@@ -1,37 +1,15 @@
 package rest.pomodoro;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PomodoroApplication implements CommandLineRunner {
-
-	@Autowired
-	private UserRepository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(PomodoroApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		repository.deleteAll();
-		User mk = new User();
-		mk.setFirstname("Mikaz");
-		mk.setLastname("Titarat");
-		mk.setScore(0);
-		repository.save(mk);
-		repository.save(new User("Mikaz","Spare",2000));
-
-		for (User user : repository.findAll()) {
-			System.out.println(user);
-		}
-		System.out.println();
-
-		for (User user : repository.findByFirstname("Mikaz")){
-			System.out.println(user);
-		}
 	}
 }
--- src/main/java/rest/pomodoro/UserRepository.java
+++ src/main/java/rest/pomodoro/UserRepository.java
@@ -2,13 +2,15 @@ package rest.pomodoro;
 
 import java.util.List;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends MongoRepository<User, String> {
     public User findById(long id);
+    public User deleteById(@Param("id")long id);
     public List<User> findByFirstname(@Param("name")String firstname);
+    public List<User> findByLastname(@Param("lastname")String lastname);
     
 }
